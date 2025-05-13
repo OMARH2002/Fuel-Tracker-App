8aab4a01ac1ced04238468be04492dfa64df2330
Omar Hany Mostafa <amorahany36002@gmail.com>
2025-05-14 00:13:42 +0300

    working code

---
+++ lib/model.dart
@@ -0,0 +1,19 @@
+class FuelEntry {
+  final String id;
+  final double liters;
+  final double km;
+  final double avg;
+  final double odometer;
+  final String date;
+  final double tankCapacity;
+
+  FuelEntry({
+    required this.id,
+    required this.liters,
+    required this.km,
+    required this.avg,
+    required this.odometer,
+    required this.date,
+    required this.tankCapacity
+  });
+}
